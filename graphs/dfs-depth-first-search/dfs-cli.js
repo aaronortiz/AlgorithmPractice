8381d0aa1f2aa6b2541b71744513f91fda262998
Aaron Ortiz <aeortiz@gmail.com>
2019-02-18 16:36:46 -0600

    DFSGraph

---
+++ graphs/dfs-depth-first-search/dfs-cli.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+
+const readlineSync = require('readline-sync');
+const DFSGraph = require('./dfs');
+let dontstahp = readlineSync.keyInYN("Let's make a graph, shall we?");
+
+if(!dontstahp) return 0;
+
+const vertexCount = readlineSync.questionInt("\nHow many vertices (nodes) does your graph have?: ");
+const edgeCount =  readlineSync.questionInt("How many edges (connections) does your graph have?: ");
+const directed = readlineSync.keyInYN("Do the edges in your graph have a single direction (directed vs undirected graph)? ");
+
+const myGraph = new DFSGraph();
+
+console.log('\n');
+
+for(let i = 0; i < vertexCount; i++){
+  myGraph.addVertex(readlineSync.question("Enter a vertex: ", { limit: /[A-Za-z]/ }));
+}
+
+console.log("\nVertices: ");
+console.log(myGraph.vertices);
+console.log('\n');
+
+for(let currEdge = 1; currEdge <= edgeCount; currEdge++){
+
+  let from = "";
+  let dest = "";
+
+  while (!(from in myGraph.vertices)) {
+    from = readlineSync.question("Enter the start vertex for edge " + currEdge + ": ");
+    if (!(from in myGraph.vertices)) {
+      console.log("Vertex " + from + " is not part of your graph. Try one of these: ");
+      console.log(myGraph.vertices);
+    }
+  }
+
+  while (!(dest in myGraph.vertices)) {
+    dest = readlineSync.question("Enter the end vertex for edge " + currEdge + ": ");
+    if (!(dest in myGraph.vertices)) {
+      console.log("Vertex " + dest + " is not part of your graph. Try one of these: ");
+      console.log(myGraph.vertices);
+    }
+  }
+  myGraph.addEdge(from, dest, directed);
+
+}
+
+dontstahp = readlineSync.keyInYN("\nShall we traverse the graph?\n");
+
+if (!dontstahp){
+  myGraph.dfs();
+}
---
+++ graphs/dfs-depth-first-search/dfs.js
@@ -0,0 +1,54 @@
+module.exports = class Graph {
+
+  constructor() {
+    this.vertices = {};
+    this.edges = {};
+    this.visited = [];    
+  }
+  /*---------------------------------------------------------------------------*/
+  addVertex(key){
+    this.vertices[key] = key;
+    console.log("Added vertex " + key);
+  }
+  /*---------------------------------------------------------------------------*/
+  addEdge(fromKey, toKey, isDirected = false){
+
+    this.edges[fromKey] = toKey;
+    
+    if(isDirected){
+      console.log ("Added edge " + fromKey +" ---> " + toKey);
+    } else {
+      this.edges[toKey] = fromKey;
+      console.log ("Added edge " + fromKey +" <--> " + toKey);
+    }
+  }
+  /*---------------------------------------------------------------------------*/
+  visit(vertex) {
+
+    console.log("Hi! I'm at " + vertex);
+    this.visited[vertex] = true;
+
+    if (vertex in this.edges) {
+      for (let eIdx in this.edges[vertex]) {
+        let next = this.edges[vertex][eIdx];
+        if (!(next in this.visited)) {
+          console.log("I'm going to " + next + " next.")
+          this.visit(next);
+        } // end if
+      } // next e
+    } // end if
+  } // end visit
+  /*---------------------------------------------------------------------------*/
+  dfs() {
+
+    this.visited = [];
+
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (!(this.vertices[i] in this.visited)) {
+        this.visit(this.vertices[i]);
+      } // endif
+    } // next i
+
+  } // end DFS  
+
+} // endclass
