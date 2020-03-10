408e730def1bbe746d8bd967b67cb48d6ca013e0
Aaron Ortiz <aeortiz@gmail.com>
2020-03-10 10:56:17 -0600

    Advanced slightly

--- python/CTCI/01.07.rotate.matrix.py
+++ python/CTCI/01.07.rotate.matrix.py
@@ -15,13 +15,14 @@
 # spiral of 4 points
 
 # Example: (7x7)
-#  .  .  .  .  .  .  .    ==>    .  .  .  .  .  .  .
-#  .  1  2  3  4  5  .    ==>    . 16  1  2  3  4  .
-#  . 16  .  .  .  6  .    ==>    . 15  .  .  .  5  .
-#  . 15  .  .  .  7  .    ==>    . 14  .  .  .  6  .
-#  . 14  .  .  .  8  .    ==>    . 13  .  .  .  7  .
-#  . 13 12 11 10  9  .    ==>    . 12 11 10  9  8  .
-#  .  .  .  .  .  .  .    ==>    .  .  .  .  .  .  .
+#  .  .  .  .  .  .  .    ==>    .  .  .  .  .  .  .  0
+#  .  1  2  3  4  5  .    ==>    . 16  1  2  3  4  .  1
+#  . 16  .  .  .  6  .    ==>    . 15  .  .  .  5  .  2
+#  . 15  .  .  .  7  .    ==>    . 14  .  .  .  6  .  3
+#  . 14  .  .  .  8  .    ==>    . 13  .  .  .  7  .  4
+#  . 13 12 11 10  9  .    ==>    . 12 11 10  9  8  .  5
+#  .  .  .  .  .  .  .    ==>    .  .  .  .  .  .  .  6
+#  0  1  2  3  4  5  6           0  1  2  3  4  5  6
 
 # store 1
 # replace 1 with 5
@@ -39,12 +40,44 @@
 # move to next inner shell if possible
 
 # An even example: (4x4)
-#  .  .  .  .    ==>    .  .  .  .
-#  .  1  2  .    ==>    . 16  1  .
-#  . 16  3  .    ==>    .  3  2  .
-#  .  .  .  .    ==>    .  .  .  .
+#  .  .  .  .    ==>    .  .  .  .  0
+#  .  1  2  .    ==>    . 16  1  .  1
+#  . 16  3  .    ==>    .  3  2  .  2
+#  .  .  .  .    ==>    .  .  .  .  3
+#  0  1  2  3           0  1  2  3
 
 # The number of layers of a n x n matrix = floor(n / 2) + n % 2
 
 # Alternately we can start from the center and move out (easier to calculate perhaps?)
+
+
+def rotateMatrix(matrix):
+
+    for layer in range(len(matrix) // 2 + len(matrix) % 2):
+        topRow = layer
+        bottomRow = len(matrix) - layer - 1
+        leftCol = layer
+        rightCol = len(matrix) - layer - 1
+
+        for x in range(leftCol, rightCol + 1):
+            pass
+
+
+if __name__ == "__main__":
+
+    matrix1 = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+    ]
+
+    rotated1 = [
+        [6, 1, 2, 3, 4],
+        [11, 7, 8, 9, 5],
+        [16, 12, 13, 14, 10],
+        [17, 18, 19, 20, 15],
+    ]
+
+    assert rotateMatrix(matrix1) == rotated1, "Matrix not rotated correctly"
 
