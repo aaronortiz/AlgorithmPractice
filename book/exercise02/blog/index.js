561707f3677dc4a4842a9c658fdac9293d4f3a06
Aaron Ortiz <aeortiz@gmail.com>
2020-06-08 09:35:33 -0500

    moved code to templating

--- book/exercise02/blog/index.js
+++ book/exercise02/blog/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = new express();
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.listen(4000, ()=>{
@@ -9,5 +13,20 @@ app.listen(4000, ()=>{
 });
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
+});
+
+app.get('/about', (req, res)=>{
+    //res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
+});
+
+app.get('/contact', (req, res)=>{
+    //res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
+});
+
+app.get('/post', (req, res)=>{
+    //res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 });
